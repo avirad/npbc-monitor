1ed4f81f456ecba5a82bdd64719b586f47181cef
avirad <avirad@mail.bg>
2017-02-21 00:46:14 +0200

    Initial draft

--- npbc_communication.py
+++ npbc_communication.py
@@ -1,5 +1,36 @@
 #!/usr/bin/python
 
+class failResponse(object):
+    pass
+
+
+class successResponse(object):
+    def __init__(self, data):
+        self.RawData = data
+
+
+class generalInformationResponse(successResponse):
+    def __init__(self, data):
+        super(generalInformationResponse, self).__init__(data)
+        self.SwVer = None
+        self.Date = None
+        self.Mode = None
+        self.State = None
+        self.Status = None
+        self.IgnitionFail = None
+        self.PelletJam = None
+        self.Tset = None
+        self.Tboiler = None
+        self.Flame = None
+        self.Heater = None
+        self.CHPump = None
+        self.BF = None
+        self.FF = None
+        self.Fan = None
+        self.Power = None
+        self.ThermostatStop = None
+
+
 class commandBase(object):
     __header = [0x5A, 0x5A]
 
@@ -35,15 +66,41 @@ class commandBase(object):
     def processResponseData(self, data):
         self.IsSuccessful = False
         response = bytearray()
-        
+
         if (len(data) < len(self.__header) + 2):
             print "Invalid response"
             return bytearray()
 
+        # check header
+        for i in range(len(self.__header)):
+            if (data[i] != self.__header[i]):
+                print "Invalid response header"
+                return bytearray()
+
+        # check length
+        if (len(data) != len(self.__header) + 1 + data[len(self.__header)]):
+            print "Invalid response length"
+            return bytearray()
+
+        for i in range(2, len(data) - 1, 1):
+            response.append(data[i])
+
+        # decrement response data values
+        for i in range(1, len(response), 1):
+            response[i] = response[i] - i + 1
+
+        # checksum validation
+        if (data[len(data) - 1] != self.__calculateCheckSum(response) + len(response) - 1):
+            print "Response checksum validation failed"
+            return bytearray()
+
+        del response[0:1]
+
+        self.IsSuccessful = True
         return response
 
     def __calculateCheckSum(self, data):
-        return (sum(data) % 256) ^ 0xFF
+        return (sum(data) & 0xFF) ^ 0xFF
 
 
 class generalInformationCommand(commandBase):
@@ -51,5 +108,12 @@ class generalInformationCommand(commandBase):
         super(generalInformationCommand, self).__init__(0x01)
 
     def getRequestData(self):
-        super(generalInformationCommand, self).processResponseData(bytearray([1, 2, 3, 4]))
         return super(generalInformationCommand, self).getRequestData(bytearray())
+
+    def processResponseData(self, response):
+        responseData = super(generalInformationCommand, self).processResponseData(response)
+
+        if (self.IsSuccessful == True):
+            return generalInformationResponse(responseData)
+        else:
+            return failResponse
--- serialworker.py
+++ serialworker.py
@@ -29,8 +29,13 @@ class SerialProcess(multiprocessing.Process):
 
         while True:
             print "serialworker..."
+
             request = npbc_communication.generalInformationCommand().getRequestData()
             print binascii.hexlify(request)
+            response = npbc_communication.generalInformationCommand().processResponseData(bytearray([0x5A, 0x5A, 0x1D, 0x16, 0x14, 0x12, 0x53, 0x07, 0x1B, 0x18, 0x1D, 0x09, 0x09, 0x0D, 0x0B, 0x8C,
+                                    0x0D, 0x0E, 0x0F, 0x4C, 0x46, 0x92, 0x13, 0x14, 0x1D, 0x16, 0x17, 0x18, 0x19, 0x1A, 0x1B, 0xB7]))
+            print binascii.hexlify(response.RawData)
+
             dbconn = sqlite3.connect(settings.DATABASE)
             dbconn.execute("INSERT INTO [BurnerLogs] ([Date]) VALUES (datetime())")
             dbconn.commit()
